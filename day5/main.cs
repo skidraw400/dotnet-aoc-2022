008a4edff8f8439cbe654b4f2f5c8e1992a59a1d
skidraw400 <skidraw400@gmail.com>
2023-05-17 17:33:50 +0200

    day5 p1 rewrite, p2 made

--- day5/main.cs
+++ day5/main.cs
@@ -2,64 +2,67 @@ internal partial class Program
 {
     private static void Main(string[] args)
     {
-        var file = File.ReadLines("./input.txt");
-        Console.WriteLine("Part 1: " + PartOne(file));
+        var file = File.ReadLines("./input.txt").ToArray();
 
-        string PartOne(IEnumerable<string> file)
+        // PARSING:
+        int max = 0;
+        int endOfStacks = 0;
+        // Determine where stacks are ending
+        foreach (var line in file)
+            if (line.IndexOf("[") != -1)
+                endOfStacks += 1;
+        // Generate stack lists
+        foreach (var cha in file[endOfStacks].Split())
+            if (cha != "")
+                max += 1;
+        List<string>[] stacks = new List<String>[max];
+        List<string>[] stacks2 = new List<String>[max];
+        for (int i = 0; i < stacks.Count(); i++)
         {
-            List<string> lines = file.ToList();
-
-            // Determine where stacks are ending
-            int endOfStacks = 0;
-            foreach (var line in lines)
-                if (line.IndexOf("[") != -1)
-                    endOfStacks += 1;
-
-            // Generate stacks 
-            List<string>[] GenStacks()
+            stacks[i] = new List<string>();
+            stacks2[i] = new List<string>();
+        }
+        // Add items to lists
+        for (int i = 0; i < endOfStacks; i++)
+        {
+            for (int u = 0; u < max; u++)
             {
-                List<List<string>> stackList = new List<List<string>>();
-                foreach (var cha in lines[endOfStacks].Split())
-                    if (cha != "")
-                        stackList.Add(new List<string>());
-                return stackList.ToArray();
-            }
-
-            var stacks = GenStacks();
-            var max = stacks.Count();
-
-            // Add items to lists
-            for (int i = 0; i < endOfStacks; i++)
-            {
-                for (int u = 0; u < max; u++)
+                char item = file[endOfStacks - 1 - i][(u) * 4 + 1];
+                if (char.IsLetterOrDigit(item))
                 {
-                    char item = lines[endOfStacks - 1 - i][(u) * 4 + 1];
-                    if (item != ' ')
-                        stacks[u].Insert(0, item.ToString());
+                    stacks[u].Insert(0, item.ToString());
+                    stacks2[u].Insert(0, item.ToString());
                 }
             }
+        }
 
-            // Move elements
-            foreach (int i in Enumerable.Range(endOfStacks + 2, lines.Count() - endOfStacks - 2))
+        // Tasks
+        // Move elements
+        foreach (int i in Enumerable.Range(endOfStacks + 2, file.Count() - endOfStacks - 2))
+        {
+            var command = file[i].Split();
+            var OldStack = int.Parse(command[3]) - 1;
+            var NewStack = int.Parse(command[5]) - 1;
+            int cnt = int.Parse(command[1]);
+            // Part 1
+            foreach (int u in Enumerable.Range(1, cnt))
             {
-                var command = lines[i].Split();
-                foreach (int u in Enumerable.Range(1, int.Parse(command[1])))
-                {
-                    var OldStack = int.Parse(command[3]) - 1;
-                    var NewStack = int.Parse(command[5]) - 1;
-                    var element = stacks[OldStack][0];
-                    stacks[OldStack].RemoveAt(0);
-                    stacks[NewStack].Insert(0, element);
-                }
+                var element = stacks[OldStack][0];
+                stacks[OldStack].RemoveAt(0);
+                stacks[NewStack].Insert(0, element);
             }
+            // Part 2
+            var elements = stacks2[OldStack].GetRange(0, cnt);
+            stacks2[OldStack].RemoveRange(0, cnt);
+            stacks2[NewStack].InsertRange(0, elements);
+        }
 
-            // Create top (first) elements string
-            string answer = "";
-            foreach (var stack in stacks)
-                if (stack.Count > 0)
-                    answer += stack[0];
-
-            return answer;
+        // Print
+        foreach(var stacklist in new List<string>[][] {stacks, stacks2}) {
+            foreach(var stack in stacklist)
+                if(stack.Count() > 0) 
+                    Console.Write(stack[0]);
+            Console.WriteLine();
         }
     }
 }
